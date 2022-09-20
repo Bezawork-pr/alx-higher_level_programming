0561e03c9864edb9db338a2a44ef8b765bfc10fe
Bezawork-pr <bezaworkalx@gmail.com>
2022-09-20 03:43:12 -0700

    fix negative value out put

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,7 +8,7 @@ gre = "and is greater than 5"
 les = "and is less than 6 and not 0"
 if int(lastdigit) > 5:
     print("{} {:d} is {:d} {}".format(strings, number, int(lastdigit), gre))
-elif int(lastdigit) < 6 and int(lastdigit) > 0:
+elif int(lastdigit) < 6 and int(lastdigit) != 0:
     print("{} {:d} is {:d} {}".format(strings, number, int(lastdigit), les))
 elif int(lastdigit) == 0:
     print("{} {:d} is {:d} and is 0".format(strings, number, int(lastdigit)))
