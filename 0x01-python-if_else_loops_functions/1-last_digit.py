159f64d084b8530dd1fc1b839f63a0fdccd33a32
Bezawork-pr <bezaworkalx@gmail.com>
2022-09-20 03:46:03 -0700

    Add negative to last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,6 +9,6 @@ les = "and is less than 6 and not 0"
 if int(lastdigit) > 5:
     print("{} {:d} is {:d} {}".format(strings, number, int(lastdigit), gre))
 elif int(lastdigit) < 6 and int(lastdigit) != 0:
-    print("{} {:d} is {:d} {}".format(strings, number, int(lastdigit), les))
+    print("{} {:d} is -{:d} {}".format(strings, number, int(lastdigit), les))
 elif int(lastdigit) == 0:
     print("{} {:d} is {:d} and is 0".format(strings, number, int(lastdigit)))
