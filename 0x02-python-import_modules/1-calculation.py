52113fe74d2016f1e6852fc33de9d379c497a21f
Bezawork-pr <bezaworkalx@gmail.com>
2022-09-22 03:52:31 -0700

    execute when main

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import calculator_1 as calc
-a = 10
-b = 5
-print(f"{a} + {b} = {calc.add(a, b)}")
-print(f"{a} - {b} = {calc.sub(a, b)}")
-print(f"{a} * {b} = {calc.mul(a, b)}")
-print(f"{a} / {b} = {calc.div(a, b)}")
+if __name__ == "__main__":
+    import calculator_1 as calc
 
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, calc.add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, calc.sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, calc.mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, calc.div(a, b)))
