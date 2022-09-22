eaf58fb3961e40963cc57ade68d4935f2833f0d0
Bezawork-pr <bezaworkalx@gmail.com>
2022-09-22 07:08:04 -0700

    Fix indentation and exit 1 if argument is not 3

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,35 +2,35 @@
 if __name__ == "__main__":
     import sys
 
-flag = 0
-a = 0
-b = 0
-if len(sys.argv) == 1:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
-for i in sys.argv:
-    if flag == 0:
-        flag = 1
-    elif flag == 1:
-        a = int(i)
-        flag = 2
-    elif flag == 2:
-        sign = i
-        flag = 3
-    elif flag == 3:
-        b = int(i)
-        if sign == "+":
-            result = a + b
-            print("{:d} + {:d} = {:d}".format(a, b, result))
-        elif sign == '-':
-            result = a - b
-            print("{:d} - {:d} = {:d}".format(a, b, result))
-        elif sign == '*':
-            result = a * b
-            print("{:d} * {:d} = {:d}".format(a, b, result))
-        elif sign == '/':
-            result = a / b
-            print("{:d} / {:d} = {:d}".format(a, b, result))
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+    flag = 0
+    a = 0
+    b = 0
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    for i in sys.argv:
+        if flag == 0:
+            flag = 1
+        elif flag == 1:
+            a = int(i)
+            flag = 2
+        elif flag == 2:
+            sign = i
+            flag = 3
+        elif flag == 3:
+            b = int(i)
+            if sign == "+":
+                result = a + b
+                print("{:d} + {:d} = {:d}".format(a, b, result))
+            elif sign == '-':
+                result = a - b
+                print("{:d} - {:d} = {:d}".format(a, b, result))
+            elif sign == '*':
+                result = a * b
+                print("{:d} * {:d} = {:d}".format(a, b, result))
+            elif sign == '/':
+                result = a / b
+                print("{:d} / {:d} = {:d}".format(a, b, result))
+            else:
+                print("Unknown operator. Available operators: +, -, * and /")
+                exit(1)
