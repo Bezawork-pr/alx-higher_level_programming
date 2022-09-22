b2fc4057f330adc77b4aabeea56825c63fbb194d
Bezawork-pr <bezaworkalx@gmail.com>
2022-09-22 02:58:55 -0700

    Add format to int

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,5 @@ def add():
     import add_0 as addition
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, addition.add(1, 2)))
+    print("{:d} + {:d} = {:d}".format(a, b, addition.add(1, 2)))
 add()
