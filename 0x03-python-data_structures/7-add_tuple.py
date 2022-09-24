6ea17d5231491140d4a83358b399412b04ebc9dd
Bezawork-pr <bezaworkalx@gmail.com>
2022-09-24 00:09:34 -0700

    fix index problem

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_c = ()
+    for i in range(2):
+        [tuple_c(x y) for x in tuple_a for y in tuple_b if len(tuple_a) == len(tuple_b)]
