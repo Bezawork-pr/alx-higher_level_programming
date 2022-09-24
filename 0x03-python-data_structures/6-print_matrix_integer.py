da75445317212777cebbdf5313d71bedff3d549a
Bezawork-pr <bezaworkalx@gmail.com>
2022-09-24 05:54:57 -0700

    Handle when function is called with no parameter

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     counter = 0
-    matrix_length = len(matrix)
-    for i in range(len(matrix)):
-        if len(matrix) == 0:
-            print("{}".format(None))
+    if (len(matrix[0]) == 0):
+        matrix_length = 0
+        print()
+        return matrix
+    else:
+        matrix_length = len(matrix)
+    for i in range(matrix_length):
         for y in matrix[i]:
             if counter != matrix_length - 1:
                 print("{:d}".format(y), end=" ")
