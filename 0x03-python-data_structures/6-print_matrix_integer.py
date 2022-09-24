e6e2a3a2dc36dd048189d9390afef46807f6ae4c
Bezawork-pr <bezaworkalx@gmail.com>
2022-09-24 05:21:34 -0700

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,12 +2,15 @@
 def print_matrix_integer(matrix=[[]]):
     counter = 0
     matrix_length = len(matrix)
-    for i in range(len(matrix)):
-        for y in matrix[i]:
-            if counter != matrix_length - 1:
-                print("{}".format(y), end=" ")
-                counter += 1
-            else:
-                print("{}".format(y))
-                counter = 0
+    if matrix_length > 0:
+        for i in range(len(matrix)):
+            for y in matrix[i]:
+                if counter != matrix_length - 1:
+                    print("{:d}".format(y), end=" ")
+                    counter += 1
+                else:
+                    print("{:d}".format(y))
+                    counter = 0
+    elif matrix_length == 0:
+        print()
     return matrix
