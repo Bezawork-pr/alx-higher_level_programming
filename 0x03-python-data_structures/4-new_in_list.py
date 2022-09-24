65a6f61ef27d291ecc35de71bb755e4178367b42
Bezawork-pr <bezaworkalx@gmail.com>
2022-09-24 00:47:44 -0700

    add return for other other cases

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx > 0 and idx < len(my_list):
+    if idx >= 0 and idx < len(my_list):
         my_copy = my_list[:]
         my_copy[idx] = element
         return my_copy
+    my_copy = my_list[:]
+    return my_copy
