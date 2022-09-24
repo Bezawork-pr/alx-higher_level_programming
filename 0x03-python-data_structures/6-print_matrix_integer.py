b70877928e9100d2b2c0094f56f898262b62af59
Bezawork-pr <bezaworkalx@gmail.com>
2022-09-24 05:29:05 -0700

    print empty if matrix is 0

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,15 +2,14 @@
 def print_matrix_integer(matrix=[[]]):
     counter = 0
     matrix_length = len(matrix)
-    if matrix_length > 0:
-        for i in range(len(matrix)):
-            for y in matrix[i]:
-                if counter != matrix_length - 1:
-                    print("{:d}".format(y), end=" ")
-                    counter += 1
-                else:
-                    print("{:d}".format(y))
-                    counter = 0
-    elif matrix_length == 0:
-        print()
+    for i in range(len(matrix)):
+        if len(matrix) == 0:
+            print()
+        for y in matrix[i]:
+            if counter != matrix_length - 1:
+                print("{:d}".format(y), end=" ")
+                counter += 1
+            else:
+                print("{:d}".format(y))
+                counter = 0
     return matrix
