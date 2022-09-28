a647e40b3c24a54e5a4985d3c47dab202f21e434
Bezawork-pr <bezaworkalx@gmail.com>
2022-09-28 09:48:00 -0700

    get average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    mulandadd = 0
+    add = 0
+    if my_list:
+        for i in my_list:
+            mulandadd += i[0] * i[1]
+            add += i[1]
+        return mulandadd/add
