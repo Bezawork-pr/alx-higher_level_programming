9fc765dbcc2c1d4293878b5fc679d6350dea0c14
Bezawork-pr <bezaworkalx@gmail.com>
2022-09-30 07:01:42 -0700

    convert roman number to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    tointeger = 0
+    previous = 4000
+    subtract = 0
+    roman_numeral = {
+            "M": 1000,
+            "D": 500,
+            "C": 100,
+            "L": 50,
+            "X": 10,
+            "V": 5,
+            "I": 1
+            }
+    if roman_string:
+        for i in roman_string:
+            if previous < roman_numeral[i]:
+                subtract = roman_numeral[i] - previous
+                tointeger -= previous
+                tointeger += subtract
+                previous = roman_numeral[i]
+            else:
+                tointeger += roman_numeral[i]
+                previous = roman_numeral[i]
+        return tointeger
+    return 0
