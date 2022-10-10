b409e55369ad055709ae5666238bdf97c7f32c09
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-10 02:40:29 -0700

    Check if value is integer or not

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,7 @@
+def safe_print_integer(value):
+    try:
+        if isinstance(value, int) is True:
+            print("{:d}".format(value))
+            return True
+    except:
+        return False
