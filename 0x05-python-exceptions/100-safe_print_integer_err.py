0c15c5dd863cb263b4d890abe300b54a383fe4aa
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-10 05:30:33 -0700

    Handle error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,9 +4,8 @@ import sys
 
 def safe_print_integer_err(value):
     checkifInt = isinstance(value, int)
-    string = "Exception: Unknown format code 'd' for object of type 'str'\n"
     try:
         print("{:d}".format(value))
-    except (TypeError, ValueError):
-        sys.stderr.write(string)
+    except (TypeError, ValueError) as err:
+        print("Exception: {}".format(err), file=sys.stderr)
     return checkifInt
