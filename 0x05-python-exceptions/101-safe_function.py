c83911ca79a944f7613364769c692df180aa1ca0
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-10 05:35:15 -0700

    Fix indentation error

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,6 +4,6 @@ import sys
 
 def safe_function(fct, *args):
     try:
-       return fct(*args)
+        return fct(*args)
     except (ValueError, TypeError, IndexError, ZeroDivisionError) as err:
         print("Exception: {}".format(err), file=sys.stderr)
