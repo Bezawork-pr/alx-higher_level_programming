a7cab820c42838bbb0121c6e6acbe564ce21ab08
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-10 02:47:01 -0700

    Include error type in except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
             if count == x:
                 break
             count += 1
-        except:
+        except IndexError:
             continue
     if x > count:
         count -= 1
--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,5 @@ def safe_print_integer(value):
         if isinstance(value, int) is True:
             print("{:d}".format(value))
             return True
-    except:
+    except ValueError:
         return False
