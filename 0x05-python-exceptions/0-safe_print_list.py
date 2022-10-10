51ee60ccdb7603905ad13d7d16fca865d29bb1f3
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-10 02:04:58 -0700

    Add exception handler in for loop

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,13 @@
 def safe_print_list(my_list=[], x=0):
     count = 1
-    try:
-        for i in my_list:
+    for i in my_list:
+        try:
             print("{}".format(i), end="")
             if count == x:
                 break
             count += 1
-    except IndexError:
-        raise IndexError
+        except:
+            continue
     if x > count:
         count -= 1
     print()
