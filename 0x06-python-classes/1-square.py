d45ee2425573d431b62a80ea51101cdb7800aa26
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-11 03:15:50 -0700

    Create private atribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Create a class"""
+
+
+class Square:
+    """Create a class Square with private attribute that is size"""
+    def __init__(self, size):
+        self.__size = size
