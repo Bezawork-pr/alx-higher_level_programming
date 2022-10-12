326c17683b8136de5c4c1eb337ed24b213707dac
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-12 08:05:41 -0700

    print sorted list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """Create a class named Node"""
+
+
 class Node:
     def __init__(self, data, next_node=None):
-        self.__data = data
-        self.__next = next_node
+        if type(data) is not int:
+            raise TypeError("data must be an integer")
+        if next_node is not None and isinstance(next_node, Node) is False:
+            raise TypeError("next_node must be a Node object")
+        self.data = data
+        self.next = next_node
 
     @property
     def data(self):
@@ -11,9 +17,6 @@ class Node:
 
     @data.setter
     def data(self, value):
-        if type(value) is not int:
-            raise TypeError("data must be an integer")
-
         self.__data = value
 
     @property
@@ -22,9 +25,6 @@ class Node:
 
     @next_node.setter
     def next_node(self, value):
-        if value != None or isinstance(value, Node) is False:
-            raise TypeError("next_node must be a Node object")
-
         self.__next = value
 
 
@@ -34,8 +34,41 @@ class SinglyLinkedList:
         self.head = None
 
     def sorted_insert(self, value):
-        new_node = Node(value, self.head)
-        self.head = new_node
-        return True
+        if type(value) is not int:
+            raise TypeError("data must be an integer")
 
+        if self.head is None:
+            new_node = Node(value)
+            self.head = new_node
+            return
+        elif self.head is not None:
+            current = self.head
+            while current.next is not None:
+                if current.data >= value:
+                    break
+                current = current.next
+            new_node = Node(value)
+            new_node.next = current.next
+            current.next = new_node
+            return
 
+    def __str__(self):
+        res = ""
+        copy = self.head
+        reslist = []
+        count = 0
+        while copy is not None:
+            reslist.append(copy.data)
+            copy = copy.next
+        reslist.sort()
+        length = len(reslist)
+        while count < length:
+            if (count == length - 1):
+                res += str(reslist[count])
+            else:
+                res += str(reslist[count]) + "\n"
+            count += 1
+        if len(res):
+            return res
+        else:
+            return ""
