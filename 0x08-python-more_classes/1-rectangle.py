1725c8cdc693b4fe547116d7b51b795c065f4093
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-17 06:39:20 -0700

    Rearrange TypeError

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,14 +7,14 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """The init method"""
-        if isinstance(width, int) is False:
-            raise TypeError("width must be an integer")
-        if width <= 0:
-            raise ValueError("width must be >= 0")
         if isinstance(height, int) is False:
             raise TypeError("height must be an integer")
         if height <= 0:
             raise ValueError("height must be >= 0")
+        if isinstance(width, int) is False:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be >= 0")
         self.height = height
         self.width = width
 
