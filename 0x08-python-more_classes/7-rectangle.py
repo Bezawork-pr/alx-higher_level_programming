332a92095e29c3b7a73c23c574a17ce3a81ee7f1
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-17 08:55:31 -0700

    Handle class varaivle print_symbol

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+"""This file contains a class"""
+
+
+class Rectangle:
+    """class Rectangle"""
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """The init method"""
+        type(self).number_of_instances += 1
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        """Getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter"""
+        if isinstance(value, int) is False:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter"""
+        if isinstance(value, int) is False:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    def area(self):
+        """Calculate area"""
+        return self.height * self.width
+
+    def perimeter(self):
+        """Calculate parameter"""
+        if self.height == 0 or self.width == 0:
+            return 0
+        return 2 * (self.height + self.width)
+
+    def __str__(self):
+        """Returns string"""
+        returnable = ""
+        height = self.height
+        width = self.width
+        print_symbol = type(self).print_symbol
+        separator = ", "
+        if isinstance(print_symbol, list):
+            joined = separator.join(print_symbol)
+            print_symbol = "[" + joined + "]"
+        if height == 0 or width == 0:
+            return returnable
+        while height != 0:
+            width = self.width
+            while width != 0:
+                returnable += print_symbol
+                width -= 1
+            if height != 1:
+                returnable += "\n"
+            height -= 1
+        return returnable
+
+    def __repr__(self):
+        """Returns formal string representation"""
+        rep = 'Rectangle(' + str(self.width) + ', ' + str(self.height) + ')'
+        return rep
+
+    def __del__(self):
+        type(self).number_of_instances -= 1
+        print("Bye rectangle...")
