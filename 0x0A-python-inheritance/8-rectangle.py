fe928559ae5f778941dead928f87c8f228abe368
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-24 07:13:57 -0700

    Import instead of rewrite

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,26 +2,7 @@
 """
 This file contains a class
 """
-
-
-class BaseGeometry:
-    """
-    class BaseGeometry
-    """
-    def __init__(self):
-        pass
-
-    def area(self):
-        """area func"""
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """integer_validator"""
-        if type(value) is not int:
-            raise TypeError('{} must be an integer'.format(name))
-        if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
-        return value
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
