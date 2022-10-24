36a62710d4c538bd412d1c6606ec5581f60b2411
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-24 03:12:52 -0700

    Add more documentation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" This file contains one function that looksup
+a class attribute and method."""
+
+
 def lookup(obj):
     """Return all attributes and methods"""
     return dir(obj)
