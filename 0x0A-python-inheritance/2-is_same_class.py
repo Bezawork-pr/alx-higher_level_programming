ceeef611a17fea225d627cc3cef5115297611908
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-24 04:58:05 -0700

    return True if object is instance of class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This file cointains funcition is_same_class
+"""
+
+
+def is_same_class(obj, a_class):
+    """Return True if object is instance of class provided"""
+    return (isinstance(obj, a_class))
