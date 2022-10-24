ed17f64c60e2f1d123740c0acf4e777d747b717e
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-24 07:23:31 -0700

    Make class printable

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This file contains a class
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    class Rectangle
+    """
+    def __init__(self, width, height):
+        """init fun"""
+        super().__init__
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """area"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """magic func str"""
+        my_string = "[Rectangle] "
+        my_string += str(self.__width) + "/" + str(self.__height)
+        return my_string
