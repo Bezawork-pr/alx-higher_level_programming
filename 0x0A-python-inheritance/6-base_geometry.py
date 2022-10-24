42c66d2a9b8ee077a32c74119b8f22109d87c211
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-24 05:59:16 -0700

    Raise an error

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This file contains a class"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception('area() is not implemented')
