2d27a559337359d16bf55515233e71cb79e549d2
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-24 07:05:50 -0700

    Add a sub class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This file contains a class
+"""
+
+
+class BaseGeometry:
+    """
+    class BaseGeometry
+    """
+    def __init__(self):
+        pass
+
+    def area(self):
+        """area func"""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """integer_validator"""
+        if type(value) is not int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
+        return value
+
+
+class Rectangle(BaseGeometry):
+    """
+    class Rectangle
+    """
+    def __init__(self, width, height):
+        """init fun"""
+        super().__init__
+        self.integer_validator(self, width)
+        self.integer_validator(self, height)
+        self.__width = width
+        self.__height = height
