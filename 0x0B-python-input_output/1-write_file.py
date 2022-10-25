27418ddf0c63f4c600414530e9578cc20c47376b
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-25 04:01:46 -0700

    Add doc string and function description

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+This file contains one function
+"""
+
+
 def write_file(filename="", text=""):
+    """This function writes to files and return length of written text"""
     with open(filename, "w", encoding="utf-8") as f:
         file_write = f.write(text)
     return len(text)
