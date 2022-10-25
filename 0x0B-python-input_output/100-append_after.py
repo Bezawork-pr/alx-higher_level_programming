9d05c18d4f08f248d69dde0a599d1f8ad0640a2b
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-25 09:34:23 -0700

    Search string and append given text in a file

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This file contains a function that appends to file
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Append after search"""
+    with open(filename, "r", encoding="utf-8") as f:
+        read = f.readlines()
+    with open(filename, "w", encoding="utf-8") as clear:
+        pass
+    with open(filename, "w", encoding="utf-8") as modify:
+        for line in read:
+            modify.write(line)
+            for word in line.split():
+                check_word = ""
+                for letter in word:
+                    if word == search_string:
+                        break
+                    check_word += letter
+                    if check_word == search_string:
+                        modify.write(new_string)
+                if word == search_string:
+                    modify.write(new_string)
