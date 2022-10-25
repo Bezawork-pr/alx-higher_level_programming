ac21947095d092aba45c63326a0f7a02ed9c6532
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-25 04:56:53 -0700

    Save to json file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This file contains one function
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Function saves to json file"""
+    with open(filename, "w", encoding="utf-8") as f:
+        write_tofile = json.dump(my_obj, f)
