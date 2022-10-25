b6cfbe8e69d5d6d57f807afe0dad3e966fb9b488
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-25 03:42:24 -0700

    Remove extra line

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@ This file cointains function read_file that reads from file
 
 def read_file(filename=""):
     """read_file func read from file and prints to STDOUT"""
-    with open(filename, "r", encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         read_data = f.read()
-    print("{}".format(read_data))
+    print("{}".format(read_data), end="")
