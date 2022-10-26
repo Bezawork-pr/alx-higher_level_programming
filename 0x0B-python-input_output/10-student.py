349f20428aac0164e9a13d793ee0d0d18613d6fc
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-26 01:01:37 -0700

    Retrive dict repesentation

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -8,11 +8,22 @@ class Student:
     def __init__(self, first_name, last_name, age):
         """init function"""
         self.first_name = first_name
-        self.lastname = last_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         """retrieves a dictionary representation"""
-        if type(attrs) == list and type(attrs[0]) == str:
-            return attrs.__name__
-        return self.__dict__.copy()
+        my_dict = {}
+        if attrs is None:
+            return dict(reversed(list(self.__dict__.items())))
+        for i in range(len(attrs)):
+            if attrs[i] == "first_name":
+                my_dict["first_name"] = self.first_name
+            elif attrs[i] == "last_name":
+                my_dict["last_name"] = self.last_name
+            elif attrs[i] == "age":
+                my_dict["age"] = self.age
+            else:
+                pass
+        dict_reversed = dict(reversed(list(my_dict.items())))
+        return dict_reversed
