faa95778177a7d0f03a73de3454017ebcd868c87
Bezawork-pr <bezaworkalx@gmail.com>
2022-10-27 02:29:47 -0700

    Create class base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This file contains class Base
+"""
+
+
+class Base:
+    """class Base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """instantiate"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
