c8f022a4ae7bd9b61c6595c01bbb31951473fb93
Bezawork-pr <bezaworkalx@gmail.com>
2022-11-29 03:15:08 -0800

    Change name of repeated colon

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,6 @@
--- Write a script that lists the number of records 
-SELECT score, count(score)
+-- Write a script that lists the number of records
+-- change count(score) to num for colomn name
+SELECT score, count(score) AS number
 FROM second_table
 GROUP BY score
 ORDER BY score DESC
