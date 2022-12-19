d6601ec1e96834b81cc90b06bea8a6e265586005
Bezawork <bezaworkalx@gmail.com>
2022-12-19 12:05:15 +0000

    Print x number of times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const printnum = process.argv[2];
+if (!printnum) {
+  console.log('Missing number of occurrences');
+}
+for (let i = 0; i < printnum; i++) {
+  console.log('c is fun');
+}
