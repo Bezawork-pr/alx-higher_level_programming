a42443ccfdd014ac8de495ee4dc705e789eceebe
Bezawork-pr <bezaworkalx@gmail.com>
2022-12-21 04:43:41 -0800

    Fix pycodestyle errors

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ import MySQLdb
 import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1],
-                        password=sys.argv[2], database=sys.argv[3])
+                         password=sys.argv[2], database=sys.argv[3])
     c = db.cursor()
     c.execute("""SELECT * FROM states""")
     for i in c:
