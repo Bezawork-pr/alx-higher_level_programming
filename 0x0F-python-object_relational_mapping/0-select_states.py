0a2f6ea518b49cbfe887e4cf953cad9beffdb93a
Bezawork-pr <bezaworkalx@gmail.com>
2022-12-21 04:19:45 -0800

    Print one by one and fix pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 import MySQLdb
-db=MySQLdb.connect(host='localhost',port=3306,password='root',database='hbtn_0e_0_usa')
+import sys
+db = MySQLdb.connect(host=sys.argv[1], port=3306,
+                     password=sys.argv[2], database=sys.argv[3])
 c = db.cursor()
 c.execute("""SELECT * FROM states""")
-print(c.fetchall())
+for i in c:
+    print(i)
