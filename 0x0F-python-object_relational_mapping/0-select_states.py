8df256297630b415aef36ef7a38ab7f99c025f84
Bezawork-pr <bezaworkalx@gmail.com>
2022-12-21 04:21:58 -0800

    Change from host to user

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import MySQLdb
 import sys
-db = MySQLdb.connect(host=sys.argv[1], port=3306,
+db = MySQLdb.connect(user=sys.argv[1], port=3306,
                      password=sys.argv[2], database=sys.argv[3])
 c = db.cursor()
 c.execute("""SELECT * FROM states""")
