0af27f34b91a78b776a3d848b8c9b03643035c56
Bezawork-pr <bezaworkalx@gmail.com>
2022-12-21 06:59:46 -0800

    Protect database against sql injections

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Get listed State in the command line
+and print
+"""
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1],
+                         password=sys.argv[2], database=sys.argv[3])
+    c = db.cursor()
+    for i in sys.argv[4]:
+        if i == ";":
+            exit(1)
+    c.execute("""SELECT *
+                FROM states
+                WHERE name = '{}'""".format(sys.argv[4]))
+    [print(i) for i in c.fetchall()]
