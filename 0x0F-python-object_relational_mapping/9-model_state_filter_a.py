758a15d2d37f01030e085817e7291d3d3b20f23a
Bezawork-pr <bezaworkalx@gmail.com>
2022-12-23 02:40:56 -0800

    list all State objects that contain the letter a

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     not_texas = session.query(State).\
-        order_by(State.id).filter(~State.name.in_(['New York']))
+        order_by(State.id).filter(State.name.like('%a%'))
     for row in not_texas:
         print("{}: {}".format(row.id, row.name))
