ea33c88beb84d0af5a74e322be3d376b986a5f7c
Bezawork-pr <bezaworkalx@gmail.com>
2022-12-23 02:34:36 -0800

    List all except Texas

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+1: Take mysql username, mysql password and database
+2: Fetch the row in table states
+"""
+import sys
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1],
+                                  sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    not_texas = session.query(State).\
+        order_by(State.id).filter(~State.name.in_(['Texas']))
+    for row in not_texas:
+        print("{}: {}".format(row.id, row.name))
