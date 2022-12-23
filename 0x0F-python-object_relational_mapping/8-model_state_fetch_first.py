b8891d6ef7aa9d772626d4f90418ba2be29244a0
Bezawork-pr <bezaworkalx@gmail.com>
2022-12-23 02:11:33 -0800

    Fetch the first row from table states

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+1: Take mysql username, mysql password and database
+2: Fetch the row in table states
+"""
+import sys
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1],
+                                  sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    first_element = session.query(State.id, State.name).\
+        order_by(State.id).first()
+    print("{}: {}".format(first_element.id, first_element.name))
