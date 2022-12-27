044bdabd7385ed20248fde63306fa57d78b71a63
Bezawork-pr <bezaworkalx@gmail.com>
2022-12-27 01:33:39 -0800

    Use Sqlalchemy instead of sqldb

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 """
-1: Take mysql username, mysql password and database
-2: Fetch states name, City id and City name
+1: Take mysql username, mysql password, database, statename
+2: deletes all State objects with a name containing the letter a
 """
 import sys
-import MySQLdb
+from sqlalchemy import create_engine
 from model_state import Base, State
 from model_city import City
-
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1],
-                         password=sys.argv[2], database=sys.argv[3])
-    c = db.cursor()
-    c.execute("""SELECT states.name, cities.id, cities.name
-                FROM cities
-                JOIN states ON state_id = states.id""")
-    for row in c:
-        print("{}: ({}) {}".format(row[0], row[1], row[2]))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1],
+                                  sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(City, State).\
+        filter(City.state_id == State.id).order_by(City.id)
+    for cities, states in query:
+        print("{}: ({}) {}".format(states.name, cities.id, cities.name))
