846661934d9645de6e4bd72b55e57cf88c439c31
Bezawork-pr <bezaworkalx@gmail.com>
2022-12-27 01:15:55 -0800

    Add foreign key

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -6,7 +6,7 @@ Alchemy ORM inorder to define classes mapped to relational database
 tables
 """
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 Base = declarative_base()
 
 
@@ -16,3 +16,4 @@ class City(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(128))
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
