154b0ec82625aa596f0a3516183497d2cfe59c56
Bezawork-pr <bezaworkalx@gmail.com>
2023-01-16 06:22:26 -0800

    Get http status code without using pipe or && or ;

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -sI GET "$1" | head -n 1 | cut -d$' ' -f2
+# Answer with pipe curl -sI GET "$1" | head -n 1 | cut -d$' ' -f2
+# -s = Don't show download progress
+# -o /dev/null = don't display the body
+# -w "%{http_code}" = Write http response code to stdout after exit
+# -w "%{http_code} prints the status code
+curl -s -o /dev/null -I -w "%{http_code}" "$1"
