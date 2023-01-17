8e26e22c4b2a63155ce30c02cc0c05e4e1c09796
Bezawork-pr <bezaworkalx@gmail.com>
2023-01-17 03:33:35 -0800

    Use r.text to get ok and type

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,6 +6,5 @@ if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     content = requests.get(url, auth=('user', 'pass'))
     print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
-    print("\t- utf8 content: {}".format(content.encoding))
+    print("\t- type: {}".format(type(content.text)))
+    print("\t- content: {}".format(content.text))
