3d78a6b2000b7a1877c2c4a99d692a7ede1bdc4e
Bezawork-pr <bezaworkalx@gmail.com>
2023-02-09 02:34:44 -0800

    Use command line argument

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/people/18/';
+const url = process.argv[2];
 let count = 0;
 request(url, (err, res, body) => {
   if (err) {
     console.log(err);
   } else {
-    const films = JSON.parse(body).films;
-    for (let i = 0; i < films.length; i++) {
-      count++;
+    const films = JSON.parse(body).results;
+    for (const movie of films) {
+      for (const character of movie.characters) {
+        if (character.endsWith('18/')) { count++; }
+      }
     }
     console.log(count);
   }
