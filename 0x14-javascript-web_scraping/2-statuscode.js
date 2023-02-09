981edbe0c5761aeef1ff8c01235694f4abd380ab
Bezawork-pr <bezaworkalx@gmail.com>
2023-02-09 01:09:32 -0800

    Add string before status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 const url = process.argv[2];
 const request = require('request');
+const string = "code: "
 const requestSettings = {
   method: 'HEAD',
   url: url
 };
 request(requestSettings, function (err, response) {
-  console.log(err || response.statusCode);
+  console.log(err || string + response.statusCode);
 });
