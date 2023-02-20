3638af3dd13eb05719334f7b1f03eb380c3e8a1b
Bezawork-pr <bezaworkalx@gmail.com>
2023-02-20 06:03:34 -0800

    Write a JavaScript script that fetches from a url

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,10 @@
+const $ = window.$;
+$(function () {
+  $.ajax({
+    type: 'GET',
+    url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
+    success: function (data) {
+      $('#hello').text(data.hello);
+    }
+  });
+});
